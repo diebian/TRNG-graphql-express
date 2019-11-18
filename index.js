21c072977f3b2fdbd904bfdcc0e44f6c6b53b4e7
Diego Mamani Zanca <dmz3010@gmail.com>
2019-11-18 10:23:26 -0400

    get data by

--- index.js
+++ index.js
@@ -4,20 +4,68 @@ const app = express();
 const express_graphql = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+const { technologies } = require("./data.json");
+// console.log("technologies", technologies);
+
 const schema = buildSchema(`
 type Query {
-    message: String
-}`);
+    technology(id: Int!): Technology
+    technologies( author: String): [Technology]
+}
+
+type Mutation {
+    updateTechnologyAuthor(id: Int!, author: String! ): Technology
+}
+
+type Technology {
+    id: Int
+    name: String
+    author: String
+    repository_url: String
+}
+
+`);
+
+let getTechnology = args => {
+  let id = args.id;
+  console.log("ID: ", id);
+  // console.log("ALL: ", technologies);
+  return technologies.filter(technology => {
+    return technology.id == id;
+  })[0];
+};
+
+let getTechnologies = args => {
+  console.log("AUTHOR: ", args);
+  if (args.author) {
+    let author = args.author;
+    return technologies.filter(technology => technology.author === author);
+  } else {
+    return technologies;
+  }
+};
+
+let updateTechnologyAuthor = ({ id, author }) => {
+  technologies.map(technology => {
+    if (technology.id == id) {
+      technology.author = author;
+      return technology;
+    }
+  });
+  return technologies.filter(technology => technology.id == id)[0];
+};
 
 const root = {
-  message: () => "WTF!"
+  technology: getTechnology,
+  technologies: getTechnologies,
+  updateTechnologyAuthor
 };
 
 app.use(
   "/graphql",
   express_graphql({
     schema,
-    root,
+    rootValue: root,
     graphiql: true
   })
 );
